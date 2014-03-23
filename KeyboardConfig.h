b092e412b76a7c18c5bae0a623e36307083ccb99
Jesse Vincent <jesse@bestpractical.com>
2014-03-22 22:03:03 -0700

    Add BT variant of Model00

--- KeyboardConfig.h
+++ KeyboardConfig.h
@@ -4,10 +4,16 @@
 #define ROWS 5
 #define EEPROM_KEYMAP_LOCATION 0
 
-#define MODEL00 true
+#define MODEL00bt true
 
 #ifdef MODEL00
 static const byte colPins[COLS] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, A0 };
+static const byte rowPins[ROWS] = { A5, A4, A3, A2, A1 };
+
+#endif
+
+#ifdef MODEL00bt
+static const byte colPins[COLS] = {MOSI, MISO,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, A0 };
 static const byte rowPins[ROWS] = { A5, A4, A3, A2, A1 };
 
 #endif
