978438c2769108517a84e8b87891d35e388d92a3
Jesse Vincent <jesse@bestpractical.com>
2014-07-07 15:47:00 -0700

    the SX1509s in prototype 2 are reversed.

--- ArduinoKeyboard.ino
+++ ArduinoKeyboard.ino
@@ -44,8 +44,8 @@
 
 // Uncomment one of the four lines to match your SX1509's address
 //  pin selects. SX1509 breakout defaults to [0:0] (0x3E).
-const byte LEFT_SX1509_ADDRESS = 0x3E;  // SX1509 I2C address (00)
-const byte RIGHT_SX1509_ADDRESS = 0x3F;  // SX1509 I2C address (01)
+const byte LEFT_SX1509_ADDRESS = 0x3F;  // SX1509 I2C address (00)
+const byte RIGHT_SX1509_ADDRESS = 0x3E;  // SX1509 I2C address (01)
 //const byte SX1509_ADDRESS = 0x70;  // SX1509 I2C address (10)
 //const byte SX1509_ADDRESS = 0x71;  // SX1509 I2C address (11)
 
